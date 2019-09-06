d366a621153eaeec3b81cd8411f928518e4b0de7
Riku Järvinen <riku.jarvinen@geniem.com>
2019-09-06 16:02:28 +0200

    Remove useless files

--- scripts/create-db.sql
+++
@@ -1,4 +0,0 @@
-create database `client-asiakas`;
-CREATE USER `client-asiakas`@`%` IDENTIFIED BY 'strongpassword';
-grant all privileges on `client-asiakas`.* to `client-asiakas`@`%`;
-flush privileges;
